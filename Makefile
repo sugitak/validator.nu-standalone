cd6a13d0863e4eb8196e938d0ffee2cce2152ccb
Hirotaka Nakajima <hiro@skyblue.me.uk>
2012-08-23 15:49:21 -0400

    ~ Reduce size of output jar : Close #1

--- Makefile
+++ Makefile
@@ -5,15 +5,17 @@ all:			$(PROGRAM)
 $(PROGRAM):		validator
 				
 validator:		
+				-mkdir lib
 				make -C checker all
 				./sbt compile
 					
 clean:			
 				make -C checker clean
-				rm -rf lib/*.jar
+				-rm -rf lib/*.jar
 				
 dist-clean:		clean
 				make -C checker dist-clean
+				-rm -rf lib
 				
 run:			validator
 				./sbt run
--- project/build.scala
+++ project/build.scala
@@ -24,10 +24,23 @@ object HTMLValidator extends Build {
       mainClass in assembly := Some("org.w3.htmlvalidator.HTMLValidatorMain"),
       jarName in assembly := "validator-nu-standalone.jar",
 
-      mergeStrategy in assembly := { 
-        case "META-INF/MANIFEST.MF" => MergeStrategy.rename
-        case _ => MergeStrategy.concat 
-      }, 
+      mergeStrategy in assembly <<= (mergeStrategy in assembly) {
+        val fs = System.getProperty("file.separator")
+        (old) =>
+          {
+            case "META-INF/MANIFEST.MF" => MergeStrategy.rename
+            case r if r.startsWith("org" + fs + "jaxen" + fs + "expr" + fs + "DefaultVariableReferenceExpr.class") => MergeStrategy.concat
+            case r if r.startsWith("org" + fs + "jaxen" + fs + "expr" + fs + "PredicateSet.class") => MergeStrategy.concat
+            case r if r.startsWith("org" + fs + "jaxen" + fs + "util" + fs + "AncestorOrSelfAxisIterator.class") => MergeStrategy.concat
+            case r if r.startsWith("org" + fs + "jaxen" + fs + "function" + fs + "StringFunction.class") => MergeStrategy.concat
+            case r if r.startsWith("org" + fs + "jaxen" + fs + "JaxenHandler.class") => MergeStrategy.concat
+            case r if r.startsWith("org" + fs + "jaxen" + fs + "saxpath" + fs + "base" + fs + "TokenTypes.class") => MergeStrategy.concat
+            case r if r.startsWith("org" + fs + "jaxen" + fs + "saxpath" + fs + "base" + fs + "XPathReader.class") => MergeStrategy.concat
+            case r if r.startsWith("org" + fs + "jaxen" + fs + "saxpath" + fs + "base" + fs + "XPathLexer.class") => MergeStrategy.concat
+            case r if r.startsWith("org" + fs + "jaxen" + fs + "SimpleNamespaceContext.class") => MergeStrategy.concat
+            case x => old(x)
+          }
+      },
       
       //      test in assembly := {},
       licenses := Seq("W3C License" -> url("http://opensource.org/licenses/W3C")),
@@ -57,30 +70,30 @@ object HTMLValidator extends Build {
       resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
       resolvers += "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
       resolvers += "apache-repo-releases" at "http://repository.apache.org/content/repositories/releases/",
-      libraryDependencies += "org.mortbay.jetty" % "jetty" % "6.1.26" % "compile",
-      libraryDependencies += "org.mortbay.jetty" % "servlet-api" % "2.5-20081211" % "compile",
-      libraryDependencies += "org.mortbay.jetty" % "jetty-util" % "6.1.26" % "compile",
-      libraryDependencies += "org.mortbay.jetty" % "jetty-ajp" % "6.1.26" % "compile",
+      libraryDependencies += "org.mortbay.jetty" % "jetty" % "6.1.26" % "compile"intransitive(),
+      libraryDependencies += "org.mortbay.jetty" % "servlet-api" % "2.5-20081211" % "compile"intransitive(),
+      libraryDependencies += "org.mortbay.jetty" % "jetty-util" % "6.1.26" % "compile"intransitive(),
+      libraryDependencies += "org.mortbay.jetty" % "jetty-ajp" % "6.1.26" % "compile"intransitive(),
       libraryDependencies += "org.slf4j" % "slf4j-log4j12" % "1.5.2" % "compile",
-      libraryDependencies += "commons-codec" % "commons-codec" % "1.4",
-      libraryDependencies += "commons-httpclient" % "commons-httpclient" % "3.1",
-      libraryDependencies += "commons-logging" % "commons-logging" % "1.1.1",
-      libraryDependencies += "commons-logging" % "commons-logging-adapters" % "1.1.1" from "http://archive.apache.org/dist/commons/logging/binaries/commons-logging-1.1.1-bin.zip",
-      libraryDependencies += "commons-logging" % "commons-logging-api" % "1.1.1" from "http://archive.apache.org/dist/commons/logging/binaries/commons-logging-1.1.1-bin.zip",
-      libraryDependencies += "com.hp.hpl.jena" % "iri" % "0.5",
-      libraryDependencies += "commons-fileupload" % "commons-fileupload" % "1.2.1",
-      libraryDependencies += "rhino" % "js" % "1.7R1",
-      libraryDependencies += "xerces" % "xercesImpl" % "2.9.1",
-      libraryDependencies += "net.sourceforge.jchardet" % "jchardet" % "1.0",
-      libraryDependencies += "net.sourceforge.saxon" % "saxon" % "9.1.0.2" from "http://switch.dl.sourceforge.net/sourceforge/saxon/saxonb9-1-0-2j.zip",
-      libraryDependencies += "junit" % "junit" % "4.4",
+      libraryDependencies += "commons-codec" % "commons-codec" % "1.4"intransitive(),
+      libraryDependencies += "commons-httpclient" % "commons-httpclient" % "3.1"intransitive(),
+      libraryDependencies += "commons-logging" % "commons-logging" % "1.1.1"intransitive(),
+      libraryDependencies += "commons-logging" % "commons-logging-adapters" % "1.1.1" from "http://archive.apache.org/dist/commons/logging/binaries/commons-logging-1.1.1-bin.zip"intransitive(),
+      libraryDependencies += "commons-logging" % "commons-logging-api" % "1.1.1" from "http://archive.apache.org/dist/commons/logging/binaries/commons-logging-1.1.1-bin.zip"intransitive(),
+      libraryDependencies += "com.hp.hpl.jena" % "iri" % "0.5"intransitive(),
+      libraryDependencies += "commons-fileupload" % "commons-fileupload" % "1.2.1"intransitive(),
+      libraryDependencies += "rhino" % "js" % "1.7R1"intransitive(),
+      libraryDependencies += "xerces" % "xercesImpl" % "2.9.1"intransitive(),
+      libraryDependencies += "net.sourceforge.jchardet" % "jchardet" % "1.0"intransitive(),
+      libraryDependencies += "net.sourceforge.saxon" % "saxon" % "9.1.0.2" from "http://switch.dl.sourceforge.net/sourceforge/saxon/saxonb9-1-0-2j.zip"intransitive(),
+      libraryDependencies += "junit" % "junit" % "4.4"intransitive(),
       libraryDependencies += "xom" % "xom" % "1.1",
-      libraryDependencies += "com.sdicons.jsontools" % "jsontools-core" % "1.5",
-      libraryDependencies += "com.hp.hpl.jena" % "iri" % "0.5",
-      libraryDependencies += "com.ibm.icu" % "icu4j" % "4.4.2" from "http://download.icu-project.org/files/icu4j/4.4.2/icu4j-4_4_2.jar",
-      libraryDependencies += "com.ibm.icu" % "icu4j-charsets" % "4.4.2" from "http://download.icu-project.org/files/icu4j/4.4.2/icu4j-charsets-4_4_2.jar",
-      libraryDependencies += "antlr" % "antlr" % "validator.nu" from "http://hsivonen.iki.fi/code/antlr.jar",
-      libraryDependencies += "isorelax" % "isorelax" % "20041111" from "http://switch.dl.sourceforge.net/sourceforge/iso-relax/isorelax.20041111.zip"))
+      libraryDependencies += "com.sdicons.jsontools" % "jsontools-core" % "1.5"intransitive(),
+      libraryDependencies += "com.hp.hpl.jena" % "iri" % "0.5"intransitive(),
+      libraryDependencies += "com.ibm.icu" % "icu4j" % "4.4.2" from "http://download.icu-project.org/files/icu4j/4.4.2/icu4j-4_4_2.jar"intransitive(),
+      libraryDependencies += "com.ibm.icu" % "icu4j-charsets" % "4.4.2" from "http://download.icu-project.org/files/icu4j/4.4.2/icu4j-charsets-4_4_2.jar"intransitive(),
+      libraryDependencies += "antlr" % "antlr" % "validator.nu" from "http://hsivonen.iki.fi/code/antlr.jar"intransitive(),
+      libraryDependencies += "isorelax" % "isorelax" % "20041111" from "http://switch.dl.sourceforge.net/sourceforge/iso-relax/isorelax.20041111.zip"intransitive()))
 
 }
 
